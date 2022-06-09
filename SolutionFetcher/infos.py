0ba3d3d3732fec4b6e8a4cd17f3e088934360c77
PONGPONGz <pongponn.pp@gmail.com>
2022-06-09 13:37:18 +0700

    First commit

--- SolutionFetcher/infos.py
+++ SolutionFetcher/infos.py
@@ -1,7 +1,7 @@
 BASE_URL = 'https://www.borntodev.com/devlab'
 API_URL = BASE_URL + '/api'
-USER_ID = ''
+USER_ID = '33872'
 COOKIES = {
-    'borntodev-auth-session': '',
-    'borntodev-auth-session.sig': ''
+    'borntodev-auth-session': 'eyJwYXNzcG9ydCI6eyJ1c2VyIjp7IklEIjoiMzM4NzIiLCJ1c2VyX2xvZ2luIjoicG9uZ3Bvbm4ucHAiLCJ1c2VyX25pY2VuYW1lIjoicG9uZ3Bvbm4tcHAiLCJ1c2VyX2VtYWlsIjoicG9uZ3Bvbm4ucHBAZ21haWwuY29tIiwidXNlcl9yZWdpc3RlcmVkIjoiMjAyMi0wMS0xMiAxOTowMjowNiIsImRpc3BsYXlfbmFtZSI6InBvbmdwb25uLnBwIiwiaXNfYWRtaW4iOmZhbHNlLCJyb2xlcyI6WyJjdXN0b21lciJdLCJ1c2VyX2ltYWdlIjoiaHR0cHM6Ly9zLmdyYXZhdGFyLmNvbS9hdmF0YXIvODliODVmMTkzMDk3YTU3YjM3OTIyYmYzZDU2NzMzNTM/ZGVmYXVsdD1tcCZzaXplPTQwMCJ9fX0=',
+    'borntodev-auth-session.sig': 'hs3Uq-vGE9qZrlXq50gpPumy0h0'
 }
